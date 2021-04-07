201145c7aded28170fb5240d9961ad02778c058f
shinmototaihei <arapon4382@gmail.comarapon4382@gmail>
2021-04-07 22:40:37 +0900

    first-commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(".top").each(function(i, elem){
+    var one = $(elem).offset().top;
+    $(window).on("load scroll resize", function(){
+      var two = $(window).height();
+      var three = $(window).scrollTop();
+      var showClass = "show";
+      var timing = 50; // 50px, add to css
+      if (three >= one - two + timing){
+        $(elem).addClass(showClass);
+      } else {
+        $(elem).removeClass(showClass);
+      }
+    });
+  });
+});
