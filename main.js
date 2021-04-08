2d5db773fe1eb532559f17461cc0013533b7e10d
shinmototaihei <arapon4382@gmail.comarapon4382@gmail>
2021-04-08 23:17:03 +0900

    first-commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function(){
-  $(".top").each(function(i, elem){
+  $("#aboutme").each(function(i, elem){
     var one = $(elem).offset().top;
     $(window).on("load scroll resize", function(){
       var two = $(window).height();
@@ -14,3 +14,38 @@ $(function(){
     });
   });
 });
+
+$(function(){
+  $("#skill").each(function(i, elem){
+    var one = $(elem).offset().top;
+    $(window).on("load scroll resize", function(){
+      var two = $(window).height();
+      var three = $(window).scrollTop();
+      var showClass = "show";
+      var timing = 50; // 50px, add to css
+      if (three >= one - two + timing){
+        $(elem).addClass(showClass);
+      } else {
+        $(elem).removeClass(showClass);
+      }
+    });
+  });
+});
+
+$(function(){
+  $("#service").each(function(i, elem){
+    var one = $(elem).offset().top;
+    $(window).on("load scroll resize", function(){
+      var two = $(window).height();
+      var three = $(window).scrollTop();
+      var showClass = "show";
+      var timing = 50; // 50px, add to css
+      if (three >= one - two + timing){
+        $(elem).addClass(showClass);
+      } else {
+        $(elem).removeClass(showClass);
+      }
+    });
+  });
+});
+
