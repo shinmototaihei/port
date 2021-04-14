49b17bfe8fe7941bde435151faaa016baaa474a3
shinmototaihei <arapon4382@gmail.comarapon4382@gmail>
2021-04-14 23:35:24 +0900

    first-commit

--- main.js
+++ main.js
@@ -49,3 +49,25 @@ $(function(){
   });
 });
 
+$(function(){
+  $("#works").each(function(i, elem){
+    var one = $(elem).offset().top;
+    $(window).on("load scroll resize", function(){
+      var two = $(window).height();
+      var three = $(window).scrollTop();
+      var showClass = "show";
+      var timing = 50; // 50px, add to css
+      if (three >= one - two + timing){
+        $(elem).addClass(showClass);
+      } else {
+        $(elem).removeClass(showClass);
+      }
+    });
+  });
+});
+
+$("#hamburger").click(function () {
+    $(this).toggleClass("activ");
+    $(".menu-content2").toggleClass("opan");
+});
+
