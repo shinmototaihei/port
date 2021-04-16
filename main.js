cca7b15a92379dd0d97f5107943515f1c075d7e6
shinmototaihei <arapon4382@gmail.comarapon4382@gmail>
2021-04-16 21:59:35 +0900

    first-commit

--- main.js
+++ main.js
@@ -71,3 +71,26 @@ $("#hamburger").click(function () {
     $(".menu-content2").toggleClass("opan");
 });
 
+
+      var beforeBg = '';
+      $(window).on('load scroll touchmove', function() {
+          $scrollTop = $(window).scrollTop();
+          // 画像を切り替える位置
+          $changePoint = $('#service').offset().top;
+          if($scrollTop < $changePoint){
+            // 同じ背景画像への切替を行わないように判定
+            if(beforeBg !== 'bgA'){
+              // 新しい画像へ切替
+              $('#skill').removeClass('bgB');
+              $('#skill').addClass('bgA');
+              beforeBg = 'bgA';
+            }
+          } else {
+            if(beforeBg !== 'bgB'){
+              $('#skill').removeClass('bgA');
+              $('#skill').addClass('bgB');
+              beforeBg = 'bgB';
+            }
+          }
+      });
+
